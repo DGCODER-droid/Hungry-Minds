cb1b12a55e52e65e073164b82e711c15f7d19151
pgurung-takeo <Prakhyat@takeo.ai>
2025-09-02 14:19:39 -0400

    Used Implicit Intent for Emailing

--- app/src/main/java/com/welovecrazyquote/hungryminds/MainActivity.java
+++ app/src/main/java/com/welovecrazyquote/hungryminds/MainActivity.java
@@ -1,8 +1,10 @@
 package com.welovecrazyquote.hungryminds;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,6 +39,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent mainCoursesActivityIntent = new Intent(MainActivity.this,MainCoursesActivity.class);
                 startActivity(mainCoursesActivityIntent);            }
+        });
+
+        TextView emailTextView = findViewById(R.id.text_view_email_address);
+        emailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchEmailAppIntent = new Intent(Intent.ACTION_SENDTO);
+                launchEmailAppIntent.setData(Uri.parse("mailto:thehungryminds@gurung.com"));
+                startActivity(launchEmailAppIntent);
+            }
         });
     }
 }
